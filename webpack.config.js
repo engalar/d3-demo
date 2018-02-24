d3817b099423e529342cccdfb9a4f08dfc72c9cd
engalar <engalar@gmail.com>
2018-02-24 10:18:49 +0800

    d3 study project demo 1

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [new HtmlWebpackPlugin({
+        "template": "./src\\index.html",
+        "filename": "./index.html",
+        "hash": false,
+        "inject": true,
+        "compile": true,
+        "favicon": false,
+        "minify": false,
+        "cache": true,
+        "showErrors": true,
+        "chunks": "all",
+        "excludeChunks": [],
+        "title": "Webpack App",
+        "xhtml": true
+    })]
+};
